ddc33fc9806af440c14e72e9185e2d33696e1cab
Mainak <mainak9990@gmail.com>
2023-07-10 00:02:26 +0530

    cart additions

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import DataProvider from "./context/DataProvider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailView from "./components/details/DetailView";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<DetailView />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
         </Box>
       </BrowserRouter>
--- client/src/components/details/ActionItem.jsx
+++ client/src/components/details/ActionItem.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, styled } from "@mui/material";
 import { ShoppingCart as Cart, FlashOn as Flash } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addtoCart } from "./../../redux/actions/cartActions";
 
 const LeftContainer = styled(Box)(({ theme }) => ({
   minWidth: "40%",
@@ -28,6 +31,16 @@ const StyledButton = styled(Button)(({ theme }) => ({
 }));
 
 const ActionItem = ({ product }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [quantity, setquantity] = useState(1);
+  const { id } = product;
+
+  const addItemtoCart = () => {
+    dispatch(addtoCart(id, quantity));
+    navigate("/cart");
+  };
+
   return (
     <LeftContainer>
       <Box style={{ padding: "15px 20px", border: "1px solid #f0f0f0" }}>
@@ -37,6 +50,7 @@ const ActionItem = ({ product }) => {
       <StyledButton
         variant="contained"
         style={{ marginRight: 10, background: "#ff9f00" }}
+        onClick={() => addItemtoCart()}
       >
         <Cart />
         Add to Cart
